5d90bb71334bdced23426fb14aa610a0903cd91f
nope01x <nope01x@users.noreply.github.com>
2025-07-18 10:33:59 +0000

    qassa: 18 July 2025 17:33:46 WIB

---
+++ build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+repos_sync() {
+    rm -rf .git && rbebb
+    msgtl -t "Build Started ( <a href='https://cirrus-ci.com/task/${CIRRUS_TASK_ID}'>View on Cirrus CI</a> )"
+
+    repo init --depth=1 -u https://github.com/rducks/android -b Q
+    git clone --depth=1 https://github.com/rducks/rom.git rdx -q
+
+    mkdir -p .repo/local_manifests
+    mv rdx/q/qassa.xml .repo/local_manifests/roomservice.xml
+
+    mkdir -p ~/.config
+    mv rdx/configs/* ~/.config
+
+    repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --prune || { msgtl -t "Repo sync failed!"; exit 1; }
+
+    patch -p1 < rdx/q/0001*
+    patch -p1 < rdx/q/0003*
+    patch -p1 < rdx/q/0004*
+    patch -p1 < rdx/q/0005*
+}
+
+build() {    
+    source build/envsetup.sh
+    lunch qassa_RMX2185-user
+    mka qassa -j$(nproc --all) 2>&1 | tee build.log
+    build_status=${PIPESTATUS[0]}   
+    if [ $build_status -eq 0 ]; then
+        msgtl -t "Build completed successfully"
+        telegram-upload out/target/product/RMX2185/*_Sisu-*.zip --caption "Build Date: $(date)" --to "$IDTL"
+        msgtl -f "build.log"
+    else
+        msgtl -t "Build failed!"
+        msgtl -f "out/error.log"
+        msgtl -f "build.log"
+        exit 1
+    fi
+}
+
+case "$1" in  
+    sync)
+        repos_sync;;
+    build)
+        build;;
+    *)       
+        exit 1;;
+esac
